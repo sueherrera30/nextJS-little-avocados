5ef42c7b9146f5183e360056d270ff24e0ad8d74
sue <sue@MacBook-Pro-de-sue.local>
2020-09-20 22:54:24 -0500

    settings

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    console.log('hola, next!');
+    return (
+        <div>
+            Hola, next.js!
+        </div>
+    )
+};
+
+export default Home;
